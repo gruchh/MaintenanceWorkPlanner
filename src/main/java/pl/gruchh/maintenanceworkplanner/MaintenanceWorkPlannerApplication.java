e14ae18c82aa84cd439937315ad3b824f649562a
gruchh <kubol2@gmail.com>
2022-08-19 12:12:38 +0200

    Init commit

---
+++ src/main/java/pl/gruchh/maintenanceworkplanner/MaintenanceWorkPlannerApplication.java
@@ -0,0 +1,13 @@
+package pl.gruchh.maintenanceworkplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceWorkPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceWorkPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gruchh/maintenanceworkplanner/MaintenanceWorkPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gruchh.maintenanceworkplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceWorkPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
